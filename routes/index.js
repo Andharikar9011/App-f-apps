2661f0e9fe23b3b681c744d0a3b049f83d332114
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2022-12-04 22:11:19 +0530

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const {
+  signin,
+  signup,
+  isAdmin,
+  isAuthorized,
+  requiresignin,
+  logout,
+} = require("../controllers/auth");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/login", signin);
+
+router.get("/dashboard", requiresignin, (req, res, next) => {
+  console.log(req.cookies);
+  console.log(req.auth);
+  res.render("dashboard");
+});
+
+router.get("/signup", function (req, res, next) {
+  res.render("signup");
+});
+
+router.post("/signup", signup);
+
+router.get("/logout", logout);
+
+module.exports = router;
